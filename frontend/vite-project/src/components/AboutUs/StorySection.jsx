7a6ac076d69e6694f838d5d618cb17ace2e1166a
oluobamzy <oluobamzy@gmail.com>
2024-09-25 07:04:11 -0400

    editing to see story changes

--- frontend/vite-project/src/components/AboutUs/StorySection.jsx
+++ frontend/vite-project/src/components/AboutUs/StorySection.jsx
@@ -10,7 +10,7 @@ const StorySection = () => {
         <div className="story-text">
           <h2 className='title'>On the quest to care for you anytime, anywhere. On the quest to care for you anytime.</h2>
           <p>
-            Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor
+           We are here to serve you veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor
             Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolorUt enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure dolor
           </p>
         </div>
