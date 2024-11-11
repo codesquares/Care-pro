51853d1026a6344882b9f5c57810971f4a7b07c8
zemola <azeemolatunji@gmail.com>
2024-11-11 08:22:27 +0100

    updated banner

---
+++ frontend/vite-project/src/components/GeneralBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/components/general-banner.scss';
+const GenaralBanner = ({
+  title = 'Hire a Caregiver today!',
+  description = 'As a Carepro caregiver, you have the opportunity to make an incredible difference the minute you walk through the door, helping your clients live a happier life in their own home.',
+  buttonText = 'Hire a Caregiver',
+  imageUrl,
+  onButtonClick,
+  borderRadius = false,
+}) => {
+  return (
+    <div className={`caregiver-banner ${borderRadius ? 'border-radius' : ''}`}>
+      <div className="content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button onClick={onButtonClick}>{buttonText}</button>
+      </div>
+      <div className="image-container">
+        <img src={imageUrl} alt="Caregiver with client" />
+      </div>
+    </div>
+  );
+};
+
+export default GenaralBanner;
