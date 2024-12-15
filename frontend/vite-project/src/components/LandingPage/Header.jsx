12439eead2a6b80389b8100170cd1a581093f48e
Azeem Olatunji <azeemolatunji@gmail.com>
2024-12-15 15:28:54 +0100

    updated data

--- frontend/vite-project/src/components/LandingPage/Header.jsx
+++ frontend/vite-project/src/components/LandingPage/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../styles/components/header.scss'; 
 import video from '../../assets/Video.png'
+import { Link } from 'react-router-dom';
 import arrow from '../../assets/arrow-right.svg'
 import Button from '../atoms/Button'
 
@@ -12,8 +13,11 @@ const Header = () => {
                 <p className="header-subtitle">Hire caregivers who have been vetted, evaluated and trained to fit your caregiving needs at home.</p>
             </div>
             <div className="header-buttons">
-                    <a href="/book-caregiver" className="btn-main">Hire a Caregiver</a>
-                    <a href="/become-caregiver" className="btn-secondary">Become a Caregiver</a>
+                    {/* <a href="/book-caregiver" >Hire a Caregiver</a> */}
+                    <Link to="/book-caregiver" className="btn-main">
+                        Hire a Caregiver
+                    </Link>
+                    <Link to="/become-caregiver" className="btn-secondary" >Become a Caregiver</Link>
                 </div>
             <div className="header-video">
                 <img src={video} alt="Caregiver helping elderly" className="video-thumbnail" />
