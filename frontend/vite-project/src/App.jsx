730fd1b4a7b9b4d27493135c60fd414fb00c5208
oluobamzy <oluobamzy@gmail.com>
2025-02-16 12:51:29 -0500

    fixing build issues

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -16,12 +16,6 @@ import LoginPage from './main-app/page/LoginPage';
 import RegisterPage from './main-app/page/RegisterPage';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; 
-
-import ProtectedRoute from './main-app/components/auth/ProtectedRoute';
-import MainAppRoutes from './main-app/routes';
-import { logout } from './main-app/services/auth';
-import { AuthProvider } from './main-app/context/AuthContext';
-import Messages from './main-app/pages/Messages';
 import ContentBlog from './components/ContentfulBlog/Blog';
 import ContentBlogPost from './components/ContentfulBlog/BlogPost';
 import {BlogProvider} from './main-app/context/BlogContext';
@@ -38,7 +32,7 @@ function ScrollToTop() {
 function App() {
     return (
 
-      <AuthProvider>
+      
         <BlogProvider>
 
         <Router>
@@ -75,7 +69,7 @@ function App() {
         </Router>
 
         </BlogProvider>
-      </AuthProvider>
+      
     );
   }
   
@@ -93,16 +87,11 @@ function App() {
       '/plans',
       '/book-caregiver',
       '/become-caregiver',
-      '/login',
-      '/register',
     ];
-  
-    // Check if current path is unprotected
-    const isUnprotectedRoute = unprotectedRoutes.includes(location.pathname.toLowerCase());
   
     return (
       <div className="App">
-        {isUnprotectedRoute && <Navbar />}
+         <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -117,24 +106,7 @@ function App() {
           <Route path="/plans" element={<Plans />} />
           <Route path="/book-caregiver" element={<BookCaregiver />} />
           <Route path="/become-caregiver" element={<BecomeCaregiver />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route
-            path="/app/*"
-            element={
-              <ProtectedRoute>
-                <MainAppRoutes />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/logout"
-            element={() => {
-              logout();
-              return <Navigate to="/login" replace />;
-            }}
-          />
-        </Routes>
+          </Routes>
         <Footer />
         <ToastContainer
           position="bottom-right"
