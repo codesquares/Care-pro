af7c5af7661ea720173740a0708c797deecb843f
oluobamzy <oluobamzy@gmail.com>
2025-11-30 16:26:00 -0500

    feat: integrate backend share endpoint for Open Graph meta tags

--- node-API/__mocks__/serviceControllers.js
+++
@@ -1,9 +0,0 @@
-// Mock service controllers
-
-const webhookController = {
-  handleWebhook: jest.fn((req, res) => res.status(200).json({ status: 'success' }))
-};
-
-module.exports = {
-  webhookController
-};
